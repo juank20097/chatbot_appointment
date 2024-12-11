d32c954442877dcf18387ff4f75222f2a6d0409e
JUAN CARLOS ESTEVEZ HIDALGO <juan.estevez@iess.gob.ec>
2024-12-11 15:20:07 -0500

    add ssl to connect bdd externa

--- utilities/Connection.js
+++ utilities/Connection.js
@@ -8,23 +8,23 @@ class Connection {
             database: process.env.POSTGRES_DB_NAME,
             password: process.env.POSTGRES_DB_PASSWORD,
             port: process.env.POSTGRES_DB_PORT,
-        //    ssl: { rejectUnauthorized: false },
+            ssl: { rejectUnauthorized: false },
         });
     }
 
     // Método para ejecutar consultas SQL usando el pool
-async executeQuery(query, params = []) {
-    try {
-        // Si no hay parámetros, ejecuta solo la consulta
-        const result = params.length > 0 
-            ? await this.pool.query(query, params) 
-            : await this.pool.query(query); // Ejecutar la consulta sin parámetros
-        return result;  // Devuelve el objeto completo de resultado
-    } catch (error) {
-        console.error('Error ejecutando la consulta:', error.stack);
-        throw error;
+    async executeQuery(query, params = []) {
+        try {
+            // Si no hay parámetros, ejecuta solo la consulta
+            const result = params.length > 0
+                ? await this.pool.query(query, params)
+                : await this.pool.query(query); // Ejecutar la consulta sin parámetros
+            return result;  // Devuelve el objeto completo de resultado
+        } catch (error) {
+            console.error('Error ejecutando la consulta:', error.stack);
+            throw error;
+        }
     }
-}
 
     // Método para cerrar el pool de conexiones
     async disconnect() {
