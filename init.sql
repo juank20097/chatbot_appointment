28a24e03aedd265fffbee14efa47ead1d848fe4b
JUAN CARLOS ESTEVEZ HIDALGO <juan.estevez@iess.gob.ec>
2024-12-11 11:59:58 -0500

    remove connection docker postgres

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS dblink;
-
-DO $$ 
-BEGIN
-   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'chatbot_appointment') THEN
-      PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE chatbot_appointment');
-   END IF;
-END $$;
